0074467262a45289d494325d12f7f063a3d5f7f3
Shota Yamamota <1230124fw@gmail.com>
2017-01-27 15:23:29 +0900

    test ok

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from graphviz import Digraph
 import pandas as pd
 
-def to_dictionary(df,serc,s_col,col):
-    k = df[df['s_col'] == serc]
-    dataList = [ i for i in k[col]]
-    return {serc:dataList}
+
+def to_dictionary(df, serc, s_col, col):
+    k = df[df[s_col] == serc]
+    dataList = [i for i in k[col]]
+    name = str(serc)
+    return {name: dataList}
+
+if __name__ == '__main__':
+    df = pd.read_csv("../../TranData_headerNUM.csv")
+    data = to_dictionary(df, 4432, '会社コード_2', 'お買い上げ金額_8')
+    print(data['4432'][0])
