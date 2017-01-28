8a2a12c92e3559ec87d07f74c2510891445695e7
Shota Yamamota <1230124fw@gmail.com>
2017-01-28 23:07:31 +0900

    add code

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from graphviz import Digraph
 import pandas as pd
 
+
 class getPlot:
+
     def __init__(self, df, G):
         self.df = df
         self.G = G
+        self.root_datas = {}
+        self.roots_datas = []
 
     def to_dictionary(self, serc, s_col, col):
         k = self.df[self.df[s_col] == serc]
@@ -12,11 +16,20 @@ class getPlot:
         name = str(serc)
         return {name: dataList}
 
+    def to_roots_dictionary(self):
+        with (i,j) in self.root_datas:
+            k = j
+            e = j[1:]
+            e.append(' ')
+            geter = list(zip(k,e))
+            geter.pop()
+
+
 if __name__ == '__main__':
     df = pd.read_csv("../../TranData_headerNUM.csv")
     u = Digraph('unix', filename='unix.gv')
     u.body.append('size="8,8"')
     u.node_attr.update(color='lightblue2', style='filled')
-    c = getPlot(df,u)
-    data = c.to_dictionary( 4432, '会社コード_2', 'お買い上げ金額_8')
+    c = getPlot(df, u)
+    data = c.to_dictionary(4432, '会社コード_2', 'お買い上げ金額_8')
     print(data['4432'][0])
